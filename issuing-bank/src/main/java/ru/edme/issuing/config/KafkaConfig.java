a15150ed5f3e6f0defccdff17e1aa489eca779e1
Yuriy-Yashkov <yuryman2019@mail.ru>
2025-05-13 00:53:54 +0300

    feat: настройка docker compose на 3 сервера

--- issuing-bank/src/main/java/ru/edme/issuing/config/KafkaConfig.java
+++ issuing-bank/src/main/java/ru/edme/issuing/config/KafkaConfig.java
@@ -3,24 +3,37 @@ package ru.edme.issuing.config;
 import org.apache.kafka.clients.admin.NewTopic;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.TopicBuilder;
-import org.springframework.stereotype.Component;
 
 import java.util.Map;
 
-@Component
+@Configuration
 public class KafkaConfig {
 
     @Value("${spring.kafka.topics.card-transfer-to-processing}")
-    private String topic;
+    private String topicToProcessing;
+
+    @Value("${spring.kafka.topics.card-transfer-to-issuing}")
+    private String topicToIssuing;
 
     @Bean
-    NewTopic createTopic() {
+    NewTopic createTopicToProcessing() {
         return TopicBuilder
-                .name(topic)
+                .name(topicToProcessing)
                 .partitions(1)
-                .replicas(1)
-                .configs(Map.of("min.insync.replicas", "1"))
+                .replicas(3)
+                .configs(Map.of("min.insync.replicas", "2"))
+                .build();
+    }
+
+    @Bean
+    NewTopic createTopicToIssuing() {
+        return TopicBuilder
+                .name(topicToIssuing)
+                .partitions(1)
+                .replicas(3)
+                .configs(Map.of("min.insync.replicas", "2"))
                 .build();
     }
 }
--- processing-center/src/main/java/ru/edme/processing/config/KafkaConfig.java
+++ processing-center/src/main/java/ru/edme/processing/config/KafkaConfig.java
@@ -2,25 +2,23 @@ package ru.edme.processing.config;
 
 import org.apache.kafka.clients.admin.NewTopic;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
 import org.springframework.kafka.config.TopicBuilder;
-import org.springframework.stereotype.Component;
 
 import java.util.Map;
 
-@Component
+//@Component todo Отключил, что бы создать Топик из 1-го сервиса
 public class KafkaConfig {
 
     @Value("${spring.kafka.topics.card-transfer-to-issuing}")
     private String topic;
 
-    @Bean
+    //    @Bean
     NewTopic createTopic() {
         return TopicBuilder
                 .name(topic)
                 .partitions(1)
-                .replicas(1)
-                .configs(Map.of("min.insync.replicas", "1"))
+                .replicas(3)
+                .configs(Map.of("min.insync.replicas", "2"))
                 .build();
     }
 }
